21bc2a2e70bc9129c6a95790bca96a07dfeceda8
tqm470 <tqm470@gmail.com>
2019-09-09 13:34:39 -0300

    Added Migrations.

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TaskManagerApi.Models;
 
 namespace TaskManager.Database
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
